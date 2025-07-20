2d1aaf5000d12522e05e1b9f598b20022bec35a7
info@dgt-international.com <info@dgt-international.com>
2025-07-20 00:05:51 -0400

    updated animated typing feature

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,19 @@
 // Type Animation
+/*
 var typed = new Typed(".typing",{
     strings:["", "web Designer", "Web Developer", "Graphic Designer", "Data Scientist"],
     typeSpeed: 100,
-    BackSpeed: 60,
+    backSpeed: 60,
     loop: true
 })
+    */
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".typing",{
+        strings:["Data Scientist", "Data Analyst", "Machine Learning Engineer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        backDelay: 1200,   // Pause before erasing
+        startDelay: 300,  // Pause before typing starts
+        loop: true
+    });
+});
