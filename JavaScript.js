8079a43631b21569f070cc0188d1fdae7aea229e
info@dgt-international.com <info@dgt-international.com>
2025-07-19 23:19:38 -0400

    commiting with local files

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+// Type Animation
+var typed = new Typed(".typing",{
+    strings:["", "web Designer", "Web Developer", "Graphic Designer", "Data Scientist"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
